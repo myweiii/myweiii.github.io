17964de3860961f7f33ddf06915c8c7bc6c39273
Mingyang Wei <mywei@Mingyangs-MacBook-Pro.local>
2024-01-23 16:02:51 -0500

    Initial commit

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Excited to attend BMI 510!",
+        "excerpt":"This semester I enroll in the course BMI 510. So excited!  ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2024/01/16/BMI510.html",
+        "teaser": null
+      }]
